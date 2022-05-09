fef8c8113e8245ccb0c9685d680db3b345f83fa5
Elvis <me@elvisw.com>
2022-05-09 23:10:17 +0800

    实现：RemoteControl

---
+++ SimpleRemoteControl/Commands/GarageDoorOpenCommand.cs
@@ -0,0 +1,13 @@
+﻿using SimpleRemoteControl.Receivers;
+
+namespace SimpleRemoteControl.Commands
+{
+    public class GarageDoorOpenCommand : ICommand
+    {
+        private readonly GarageDoor _garageDoor;
+
+        public GarageDoorOpenCommand(GarageDoor garageDoor) => _garageDoor = garageDoor;    
+
+        public void Execute() => _garageDoor.Up();
+    }
+}
---
+++ SimpleRemoteControl/Invokers/SimpleRemoteControl.cs
@@ -0,0 +1,11 @@
+﻿using SimpleRemoteControl.Commands;
+
+namespace SimpleRemoteControl.Invokers
+{
+    //This is our invoker
+    public class SimpleRemoteControl
+    {
+        public ICommand Command { private get; set; }
+        public void ButtonWasPressed() => Command.Execute();
+    }
+}
